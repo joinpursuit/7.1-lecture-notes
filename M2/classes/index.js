16ad865f33147287d17af0b6ff51ea2e675443d8
Jimmy Byess <jimmy@skelios.com>
2021-03-31 11:50:15 -0400

    add getter setter example

--- M2/classes/index.js
+++ M2/classes/index.js
@@ -9,18 +9,31 @@ class TVShow {
   }
 
   async runSearchQuery() {
-    this.result = await axios.get(`https://api.tvmaze.com/search/shows?q=${this.searchParam}`)
+    this.showResult = await axios.get(`https://api.tvmaze.com/search/shows?q=${this.searchParam}`)
   }
 
-  doMoreThings() {
-    this.somethingElse = ""
+  get result() {
+    return this.rawResult.data[0].show.name
+  }
+
+  set showResult(val) {
+    return this.rawResult = val
   }
 }
 
 async function getShow() {
   let sopranos = new TVShow("sopranos")
   await sopranos.runSearchQuery()
+  console.log(sopranos.showResult)
+  console.log(sopranos.rawResult)
   console.log(sopranos.result)
-
-
 }
+
+// runSearchQuery()
+// sets a property called showResult in class
+// this executes the setter
+// setter stores the result in another property called rawResult
+// i try to access property called result
+// this executes the getter
+// getter returns transformed value
+
