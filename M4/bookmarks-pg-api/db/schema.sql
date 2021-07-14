730361a2991862b7edefb22563610dc0f206bc9e
Tristan Angieri <tristanangieri@Tristans-MacBook-Pro.local>
2021-07-14 13:04:23 -0400

    added reviews table to bookmarks_dev

--- M4/bookmarks-pg-api/db/schema.sql
+++ M4/bookmarks-pg-api/db/schema.sql
@@ -12,3 +12,15 @@ CREATE TABLE bookmarks (
     category TEXT,
     is_favorite BOOLEAN
 );
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    reviewer TEXT,
+    title TEXT,
+    content TEXT,
+    rating NUMERIC,
+    CHECK (rating >= 0 AND rating <= 5),
+    bookmark_id INTEGER REFERENCES bookmarks (id) ON DELETE CASCADE
+);
